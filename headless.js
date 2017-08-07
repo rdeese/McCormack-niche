628f00165e017812145d55b803ab22371bffd8bb
rdeese <rdeese@hmc.edu>
2017-08-07 18:38:18 -0500

    Get background noise working.

--- headless.js
+++ headless.js
@@ -2,48 +2,53 @@ const Canvas = require('canvas');
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
-const PcgRandom = require('./js/lib/pcg-random/pcg-random');
+const OpenSimplexNoise = require('open-simplex-noise').default;
+const { makeRectangle } = require('fractal-noise');
+const QuadTree = require('./js/lib/QuadTree');
 
-let VEL = 2;
+let VEL = 1;
 let GESTATION = 2;
-let NAG_SIZE = 2;
+let NAG_SIZE = 1;
 let USE_NICHE = true;
-let NICHE_WIDTHS = [100, 200];
+let NICHE_WIDTHS = [25, 50, 100];
 let NICHE_WIDTH;
-let NICHE_AREA_SIZES = [5, 10, 20, 40];
+let NICHE_AREA_SIZES = [10, 20, 40, 80];
 let NICHE_AREA_SIZE;
 let NUM_NAGS = 10;
-let MUTATION_SEVERITIES = [0.1, 0.5, 1];
+let MUTATION_SEVERITIES = [0.05, 0.1, 0.2, 0.5];
 let MUTATION_SEVERITY;
+let REPETITIONS = 1;
+let LOG_INTERVAL = 100;
 
-let random = new PcgRandom(Date.now());
 let canvas;
 
 // returns a random float drawn from an approximate standard normal distribution
 // as per the central limit theorem (std. dev. 1, mean 0)
 const normalDistRand = function () {
-  sample = (
-    random.number() + random.number() + random.number() +
-    random.number() + random.number() + random.number() -
-    3
-  ) / 3;
-  return sample;
+  let sample = 0;
+  for (let i = 0; i < 6; i++) {
+    sample += Math.random();
+  }
+  return (sample - 3) / 3
 }
 
 let main = function () {
   _.forEach(NICHE_WIDTHS, (width) => {
     _.forEach(NICHE_AREA_SIZES, (size) => {
       _.forEach(MUTATION_SEVERITIES, (severity) => {
-        NICHE_WIDTH = width;
-        NICHE_AREA_SIZE = size;
-        MUTATION_SEVERITY = severity;
-        canvas = new Canvas(1000, 1000);
-        let context = canvas.getContext("2d");
-        let w = new World(context);
-        w.run();
-        fileId = [width, size, severity].join('_');
-        let filePath = path.resolve(process.cwd(), 'images', `output_${fileId}.png`);
-        fs.writeFileSync(filePath, canvas.toBuffer());
+        for (let i = 0; i < REPETITIONS; i++) {
+          fileId = ["niche-width", width, "niche-area", size, "mutation-size", severity, "run", i].join('_');
+          console.log(`\n\n================\nSTARTING ${fileId}\n================`);
+          NICHE_WIDTH = width;
+          NICHE_AREA_SIZE = size;
+          MUTATION_SEVERITY = severity;
+          canvas = new Canvas(1000, 1000);
+          let context = canvas.getContext("2d");
+          let w = new World(context);
+          w.run();
+          let filePath = path.resolve(process.cwd(), 'images', `output_${fileId}.png`);
+          fs.writeFileSync(filePath, canvas.toBuffer());
+        }
       });
     });
   });
@@ -59,7 +64,27 @@ World.prototype = {
 		this.width = context.canvas.width;
 		this.height = context.canvas.height;
 		this.timestep = 0;
+
+    let openSimplex = new OpenSimplexNoise(Date.now());
+    let noiseFunc = (x, y) => {
+      //return (openSimplex.noise2D(x, y) + 1) / 2;
+      return openSimplex.noise2D(x, y);
+    }
+    this.noiseMaps = []
+
+    for (let i = 0; i <= 1; i += 0.5) {
+      this.noiseMaps.push(makeRectangle(this.width, this.height, noiseFunc.bind(this), { octaves: 1, persistence: i/2, frequency: 0.005 }));
+    }
+
 		this.populateNagList();
+    this.quadTree = new QuadTree({ x: 0, y: 0, width: this.width, height: this.height }, true, 7);
+	},
+
+	fracSum: function (x, y, r) {
+    x = Math.floor(x);
+    y = Math.floor(y);
+    let noiseMap = this.noiseMaps[Math.round(r*2)];
+    return noiseMap[x][y];
 	},
 
 	populateNagList: function () {
@@ -67,39 +92,56 @@ World.prototype = {
 		for (let i = 0; i < NUM_NAGS; ++i) {
       this.nagList.push(new Nag(
         0, // curvature
-        0.5*random.number(), // irrationality
-        0.3+0.2*random.number(), // fecundity,
-        0.1*random.number(), // mortality
-        (random.number()-0.5)*Math.PI*2, // offset
-        0, // niche
-        { x: random.number()*this.width, y: random.number()*this.height }, // position
-        random.number()*Math.PI*2)); // rotation
+        0.5*Math.random(), // irrationality
+        0.2+0.4*Math.random(), // fecundity,
+        0.1*Math.random(), // mortality
+        2*Math.random()*Math.PI, // offset
+        0.05*Math.random(), // niche
+        { x: Math.random()*this.width, y: Math.random()*this.height }, // position
+        Math.random()*Math.PI*2)); // rotation
 		}
 	},
 
+  updateQuadTree: function () {
+    this.quadTree.clear();
+    this.quadTree.insert(this.nagList);
+  },
+
 	step: function () {
-		if (this.nagList.length > 0) {
-			let newNagList = [];
-			let imgData = this.context.getImageData(0,0, this.width, this.height).data;
-			for (let i = 0; i < this.nagList.length; i++) {
-				// get the current nag
-				let nag = this.nagList[i];
-				nag.iteratePosition();
+    let newNagList = [];
+    let imgData = this.context.getImageData(0,0, this.width, this.height).data;
 
-				let nags = nag.surviveAndBreed(imgData);
+    this.updateQuadTree();
 
-				// if it's old enough, we draw it
-				if (nag.age > GESTATION) {
-					this.context.fillRect(nag.p.x, nag.p.y, NAG_SIZE, NAG_SIZE);
-				}
+    for (let i = 0; i < this.nagList.length; i++) {
+      // get the current nag
+      let nag = this.nagList[i];
+      nag.iteratePosition(this.fracSum.bind(this));
 
-        newNagList.push(...nags)
-			}
-			this.nagList = newNagList;
+      let nags = nag.surviveAndBreed(this.quadTree.retrieve(nag), imgData);
+
+      // if it's old enough, we draw it
+      if (nag.age > GESTATION) {
+        this.context.fillStyle = `rgb(${Math.floor(Math.pow(nag.delta, 1.5)*255*10)}, 0, 0)`;
+        this.context.fillRect(nag.x, nag.y, NAG_SIZE, NAG_SIZE);
+      }
+
+      newNagList.push(...nags)
+    }
+
+    this.nagList = newNagList;
+
+    if (this.timestep % LOG_INTERVAL == 0) {
+      let population = this.nagList.length;
       let avgF = this.nagList.reduce((sum, nag) => { return sum += nag.f }, 0)/this.nagList.length;
       let avgNiche = this.nagList.reduce((sum, nag) => { return sum += nag.delta }, 0)/this.nagList.length;
       let avgM = this.nagList.reduce((sum, nag) => { return sum += nag.m }, 0)/this.nagList.length;
-		}
+      let avgR = this.nagList.reduce((sum, nag) => { return sum += nag.r }, 0)/this.nagList.length;
+      let avgRo = this.nagList.reduce((sum, nag) => { return sum += nag.ro }, 0)/this.nagList.length;
+      console.log(`Population: ${population} Fertility: ${avgF.toFixed(2)} Mortality: ${avgM.toFixed(2)} Curvature ${avgRo.toFixed(2)} Irrationality: ${avgR.toFixed(2)} Niche preference: ${avgNiche.toFixed(2)}`)
+    }
+
+    this.timestep += 1;
 	},
 
 	run: function () {
@@ -119,34 +161,47 @@ Nag.prototype = {
 		this.ro = this.bound(ro, -1, 1);
 		this.r =  this.bound(r, 0, 1);
 		this.f =  this.bound(f, 0, 1);
-		this.m =  this.bound(m, 0, 1);
+		this.m =  this.bound(m, 0.001, 1);
 		this.phi = phi;
     this.delta = this.bound(delta, 0, 1);
 		// end genome
 
-		this.p = {
-			x: pos.x,
-			y: pos.y
-		};
+		this.x = pos.x;
+    this.y = pos.y;
+
 		this.theta = theta;
-		this.dthetadt = 0;
 
 		// a baby!
 		this.age = 0;
 	},
 
-	surviveAndBreed: function (imgData) {
+  isCollidingWithNeighbor: function (nearbyNags) {
+    for (let neighbor of nearbyNags) {
+      if (
+        Math.abs(neighbor.x - this.x) < 1 &&
+        Math.abs(neighbor.y - this.y) < 1 && 
+        this.age < neighbor.age
+      ) {
+        return true;
+      }
+    }
+  },
+
+	surviveAndBreed: function (nearbyNags, imgData) {
     let probs = this.deathAndReproductionProb(imgData);
 		// die?
-		if (this.outOfBounds() ||
-				this.collision(imgData) ||
-				random.number() < probs.death) {
+    if (
+      this.outOfBounds() ||
+      (this.age > GESTATION && this.isCollidingWithNeighbor(nearbyNags)) ||
+      Math.random() < probs.death ||
+      this.collision(imgData)
+    ) {
 			return [];
 		} else {
 			this.returnedNags = [this];
 		}
 		// reproduce?
-		if (random.number() < probs.reproduction) {
+		if (this.age > GESTATION && Math.random() < probs.reproduction) {
 			this.returnedNags.push(this.bearOffspring());
 		}
 		// get older
@@ -179,13 +234,6 @@ Nag.prototype = {
     }
   },
 
-	// attempt at the fracSum function described in the paper. I assum the second argument
-	// to the function is determining the number of octaves, but I could be totally wrong.
-	fracSum: function (p, octaves) {
-		PerlinSimplex.noiseDetail(Math.floor(octaves), 0.5);
-		return PerlinSimplex.noise(p.x, p.y);
-	},
-
 	bound: function (num, min, max) {
 		return Math.max(Math.min(num, max), min);
 	},
@@ -193,67 +241,68 @@ Nag.prototype = {
   // using 4 pixels on the canvas that the nag is over, determines if the
   // nag is colliding with a line
 	collision: function (data) {
-		let nextX = this.p.x + VEL*Math.cos(this.theta);
-		let nextY = this.p.y + VEL*Math.sin(this.theta);
-		let p1 = data[4*canvas.width*Math.floor(nextY)+4*Math.floor(nextX)+3];
-		let p2 = data[4*canvas.width*Math.floor(nextY)+4*Math.ceil(nextX)+3];
-		let p3 = data[4*canvas.width*Math.ceil(nextY)+4*Math.floor(nextX)+3];
-		let p4 = data[4*canvas.width*Math.ceil(nextY)+4*Math.ceil(nextX)+3];
-		return (p1+p2+p3+p4)>190;
+		let nextX = this.x + VEL*Math.cos(this.theta);
+		let nextY = this.y + VEL*Math.sin(this.theta);
+    let alphaSum = 0;
+
+    for (let x = Math.floor(nextX); x <= Math.ceil(nextX); x++) {
+      for (let y = Math.floor(nextY); y <= Math.ceil(nextY); y++) {
+        alphaSum += data[4*canvas.width*y+4*x+3];
+      }
+    }
+
+		return alphaSum>190;
 	},
 
   // returns the ratio of 'inked' area to total area around the nag. "area around
   // the nag" is a square with side length given by the global constant NICHE_AREA_SIZE
   localDensity: function (data) {
-    xStart = Math.floor(this.p.x-NICHE_AREA_SIZE/2);
-    xEnd = Math.ceil(this.p.x+NICHE_AREA_SIZE/2);
-    yStart = Math.floor(this.p.y-NICHE_AREA_SIZE/2);
-    yEnd = Math.ceil(this.p.y+NICHE_AREA_SIZE/2);
+    xStart = Math.floor(this.x-NICHE_AREA_SIZE/2);
+    xEnd = Math.ceil(this.x+NICHE_AREA_SIZE/2);
+    yStart = Math.floor(this.y-NICHE_AREA_SIZE/2);
+    yEnd = Math.ceil(this.y+NICHE_AREA_SIZE/2);
     var totalArea = 0;
-    // since a single "black pixel" is sometimes averaged over multiple pixels, we'll
-    // try adding up the total opacity within the area, and getting a count by
-    // dividing by 255
-    var inkedOpacityCount = 0;
+    var inkedPixelCount = 0;
     for (var x = xStart; x < xEnd; x++) {
       for (var y = yStart; y < yEnd; y++) {
-        inkedOpacityCount += data[4*canvas.width*y+4*x+3];
+        if (data[4*canvas.width*y+4*x+3] > 0) {
+          inkedPixelCount += 1;
+        }
         totalArea++;
       }
     }
-    var density = (inkedOpacityCount / 255) / totalArea;
+    var density = inkedPixelCount / totalArea;
     return density;
   },
 
 	outOfBounds: function () {
-		return this.p.x < 0 || this.p.y < 0 || this.p.x > canvas.width || this.p.y > canvas.height;
+		return this.x < 0 || this.y < 0 || this.x > canvas.width || this.y > canvas.height;
 	},
 
 	bearOffspring: function () {
-		return new Nag(this.ro+normalDistRand()*MUTATION_SEVERITY,
-									 this.r+normalDistRand()*MUTATION_SEVERITY,
-									 this.f+normalDistRand()*MUTATION_SEVERITY,
-									 this.m+normalDistRand()*MUTATION_SEVERITY,
-									 this.phi+normalDistRand()*MUTATION_SEVERITY,
-                   this.delta+normalDistRand()*MUTATION_SEVERITY,
-									 this.p,
-									 this.theta+this.phi);
+    return new Nag(
+      this.ro, //+normalDistRand()*MUTATION_SEVERITY,
+      this.r+normalDistRand()*MUTATION_SEVERITY,
+      this.f+normalDistRand()*MUTATION_SEVERITY,
+      this.m+normalDistRand()*MUTATION_SEVERITY,
+      this.phi+normalDistRand()*MUTATION_SEVERITY,
+      this.delta+normalDistRand()*MUTATION_SEVERITY,
+      { x: this.x, y: this.y },
+      this.theta+this.phi
+    );
 	},
 
 	// update the Nag's position based on its heading (theta) and current position.
 	// coordinate space used has the 0 heading pointing ->, and positive increase
 	// in heading goes counterclockwise, like graph quadrants.
-	iteratePosition: function () {
-		// set new curvature
-		// k is a global constant. see formula (1) in the paper. TODO set k
-		//this.dthetadt = this.ro + Math.pow(this.fracSum(this.p, k*this.r), 0.89*Math.pow(this.r, 2));
-		this.dthetadt = this.ro + normalDistRand()*this.r;
-		// FIXME Arbitrarily bounding dthetadt by -1 and 1 rads/frame
-    // this.dthetadt = this.bound(this.dthetadt, -1, 1);
+	iteratePosition: function (fracSum) {
+    let randomChange = fracSum(this.x, this.y, this.r) * Math.pow(this.r, 2);
+		let dthetadt = this.ro + randomChange;
 
 		// update the heading and position of the nag
-		this.theta += this.dthetadt;
-		this.p.x += VEL*Math.cos(this.theta);
-		this.p.y += VEL*Math.sin(this.theta);
+		this.theta += dthetadt;
+		this.x += VEL*Math.cos(this.theta);
+		this.y += VEL*Math.sin(this.theta);
 	}
 }
 
